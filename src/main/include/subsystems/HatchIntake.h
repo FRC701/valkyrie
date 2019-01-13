c8839a8ad9c248e11290719097d6f13abaa90bdf
Julie Tolentino <juliet@vandenrobotics.com>
2019-01-13 13:03:25 -0800

    Basic Setup for the HatchIntake Subsystem
    Set up talons and pneumatics for RobotMap (IDS are just filler for now)
    Set up the basics for a hatch intake subsystem
    Added basic commands to test out basic code
    Added HatchIntake to Robot

---
+++ src/main/include/subsystems/HatchIntake.h
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+#ifndef SRC_SUBSYSTEMS_HATCHINTAKE_H_
+#define SRC_SUBSYSTEMS_HATCHINTAKE_H_
+#pragma once
+
+#include <frc/commands/Subsystem.h>
+#include "RobotMap.h"
+#include "frc/DoubleSolenoid.h"
+#include "ctre/Phoenix.h"
+
+class HatchIntake : public frc::Subsystem {
+ private:
+  // It's desirable that everything possible under private except
+  // for methods that implement subsystem capabilities
+  static const char kSubsystemName[];
+  static std::shared_ptr<HatchIntake> self;
+  frc::DoubleSolenoid mPuncher;
+  WPI_TalonSRX mPivot;
+ public:
+  HatchIntake();
+  static std::shared_ptr<HatchIntake> getInstance();
+  void InitDefaultCommand() override;
+  void Engage();
+  void Disengage();
+  void Pivot(double speed);
+};
+#endif 
