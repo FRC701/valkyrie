3376ca0952817f2a1ddc39a1c8b3a688c10c0d88
Nicolas Schallock <nics@vandenrobotics.com>
2019-01-26 16:59:43 -0800

    Robot now follows and approaches the target

--- src/main/cpp/commands/limeLight.cpp
+++ src/main/cpp/commands/limeLight.cpp
@@ -13,8 +13,9 @@
 
 bool driveF = false;
 bool driveB = false;
-bool rotationL = false;
-bool rotationR = false;
+bool rotation = false;
+double tx = 0.0;
+double ta = 0.0;
 
 limeLight::limeLight() {
   // Use Requires() here to declare subsystem dependencies
@@ -30,53 +31,55 @@ void limeLight::Execute() {
 
   std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
   double targetOffsetAngle_Horizontal = table->GetNumber("tx",0.0);
-  double targetOffsetAngle_Vertical = table->GetNumber("ty",0.0);
+  double targetOffsetAngle_HorizontalL = table->GetNumber("tx0",0.0);
+  double targetOffsetAngle_HorizontalS = table->GetNumber("tx1",0.0);
   double targetArea = table->GetNumber("ta",0.0);
-  double targetSkew = table->GetNumber("ts",0.0);
+  double targetAreaL = table->GetNumber("ta0",0.0);
+  double targetAreaS = table->GetNumber("ta1",0.0);
 
-  if (targetOffsetAngle_Horizontal < 0) {
-    //rotationL = true;
-    //rotationR = false;
-    Chassis::getInstance()->LimeLightDrive(-1,1);
+  if (targetOffsetAngle_Horizontal < -4) {
+    rotation = true;
+    Chassis::getInstance()->LimeLightDrive(0.025,-0.025);
   }
-  else if (targetOffsetAngle_Horizontal > 0) {
-    //rotationL = false;
-    //rotationR = true;
-    Chassis::getInstance()->LimeLightDrive(1,-1);
+  if (targetOffsetAngle_Horizontal > -2) {
+    rotation = true;
+    Chassis::getInstance()->LimeLightDrive(-0.025,0.025);
   }
 
-  else {
-    //rotationL = false;
-    //rotationR = false;
+  if (targetOffsetAngle_Horizontal < -2 && targetOffsetAngle_Horizontal > -4) {
+    rotation = false;
     Chassis::getInstance()->LimeLightDrive(0,0);
   }
-/*
-  if (targetArea < 15) {
+
+  if (targetArea < 3) {
     driveF = true;
     driveB = false;
   }
   
-  else if (targetArea > 25) {
+  if (targetArea > 3.5) {
     driveF = false;
     driveB = true;
   }
 
-  else {
+  if (targetArea < 3.5 && targetArea > 3) {
     driveF = false;
-    driveB = true;
+    driveB = false;
   }
 
-  if (driveF == true && rotationL == true) {
-    Chassis::getInstance()->LimeLightDrive(0.5,1);
+  if (rotation == true) {
+    
   }
-  else if (driveF == true && rotationL == false && rotationR == true) {
-    Chassis::getInstance()->limeLightDrive(1,0.5);
+  if (rotation == false) {
+    if (driveF == true) {
+      Chassis::getInstance()->LimeLightDrive(0.05,0.05);   
+    }
+    if (driveB == true) {
+      Chassis::getInstance()->LimeLightDrive(-0.05,-0.05);
+    }
+    if (driveF == false && driveB == false) {
+      Chassis::getInstance()->LimeLightDrive(0,0);
+    }
   }
-  else if (driveF == false && driveB == true && rotationL == true) {
-    Chassis::getInstance()->limeLightDrive(-1,-0.5);
-  }
-  else if (driveF)
-  */
 }
 
 // Make this return true when this Command no longer needs to run execute()
