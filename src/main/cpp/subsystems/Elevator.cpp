e60271ecad74e13f7f0f77feaf473ca95140efb4
Julie Tolentino and Darius Azure <juliet@vandenrobotics.com>
2019-02-03 16:49:16 -0800

    Added SoftLimit to left side of elevator

--- src/main/cpp/subsystems/Elevator.cpp
+++ src/main/cpp/subsystems/Elevator.cpp
@@ -77,6 +77,7 @@ void Elevator::SetUpTalons(){
 	mLeftElevator.SetInverted(true);
 	mLeftElevator.Follow(mRightElevator);
 	mLeftElevator.ConfigForwardSoftLimitEnable(true, kTimeout_10Millis);
+	mLeftElevator.ConfigForwardSoftLimitThreshold(kForwardSoftLimit);
 }
 
 bool Elevator::IsRevLimitSwitchClosed() {
