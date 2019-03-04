858c27a226d53234e2c72fbe47a5dde2849cce53
Tamia <tamiav@vandenrobotics.com>
2019-03-03 16:01:34 -0800

    Framework for climber commands

---
+++ src/main/cpp/commands/StageTwoClimb.cpp
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "commands/StageTwoClimb.h"
+#include "commands/DriveClimb.h"
+#include "commands/ClimberEngage.h"
+#include "commands/ClimberDisengage.h"
+#include "commands/MotorClimb.h"
+
+Climb::Climb() {
+  AddSequential(new MotorClimb());
+  AddSequential(new DriveClimb());
+  AddSequential(new MotorClimb());
+}
