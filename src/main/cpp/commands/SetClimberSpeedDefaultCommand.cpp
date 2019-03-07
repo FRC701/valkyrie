f4f23e1af44c4471ff4ebe5e57fe634d8961b1de
Darius Azure <majorminor777@hotmail.com>
2019-03-07 11:21:56 -0800

    Climber: SetClimberDefaultCommands

---
+++ src/main/cpp/commands/SetClimberSpeedDefaultCommand.cpp
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "commands/SetClimberSpeedDefaultCommand.h"
+#include "subsystems/Climber.h"
+
+SetClimberSpeedDefaultCommand::SetClimberSpeedDefaultCommand() {
+  // Use Requires() here to declare subsystem dependencies
+  Requires(Climber::getInstance().get());
+ }
+
+// Called once when the command executes
+void SetClimberSpeedDefaultCommand::Initialize() {
+  Climber::getInstance()->SetSpeedDefaultCommand();
+}
