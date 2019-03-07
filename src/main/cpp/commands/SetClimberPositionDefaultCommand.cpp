a6333a28ef97429e9cb672940eac9c2a1e0c69e3
Darius Azure <majorminor777@hotmail.com>
2019-03-07 11:21:56 -0800

    Climber: SetClimberDefaultCommands

---
+++ src/main/cpp/commands/SetClimberPositionDefaultCommand.cpp
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "commands/SetClimberPositionDefaultCommand.h"
+#include "subsystems/Climber.h"
+
+SetClimberPositionDefaultCommand::SetClimberPositionDefaultCommand() {
+  // Use Requires() here to declare subsystem dependencies
+  Requires(Climber::getInstance().get());
+}
+
+// Called once when the command executes
+void SetClimberPositionDefaultCommand::Initialize() {
+  Climber::getInstance()->SetPositionDefaultCommand();
+}
