71820cc9e6c7cf926f84e9253dc87f9ea6fbcf72
Nicolas Schallock <nics@vandenrobotics.com>
2019-03-08 18:17:42 -0800

    Climber: ClimberDrive
    Use the joystick to drive the robot with the climber is up.
    
    Disabled.

---
+++ src/main/cpp/commands/ClimberDrive.cpp
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "commands/ClimberDrive.h"
+#include "subsystems/Climber.h"
+#include "OI.h"
+
+ClimberDrive::ClimberDrive() {
+  // Use Requires() here to declare subsystem dependencies
+  // eg. Requires(Robot::chassis.get());
+  Requires(Climber::getInstance().get());
+}
+
+// Called just before this Command runs the first time
+void ClimberDrive::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void ClimberDrive::Execute() {  
+ // double right = OI::getInstance()->getDriverRightYAxis();
+  //Climber::getInstance()->DriveClimb(right);
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool ClimberDrive::IsFinished() { return false; }
+
+// Called once after isFinished returns true
+void ClimberDrive::End() {}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void ClimberDrive::Interrupted() {}
