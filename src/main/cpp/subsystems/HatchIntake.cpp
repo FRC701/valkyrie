6cba07a3b77cff5c534bf7c3d641def82a47fa43
Nicolas Schallock <nics@vandenrobotics.com>
2019-03-17 09:36:05 -0700

    HatchIntake: Increasing velocity and acceleration
    Note: SensorPhase is set to practice bot here

--- src/main/cpp/subsystems/HatchIntake.cpp
+++ src/main/cpp/subsystems/HatchIntake.cpp
@@ -105,7 +105,7 @@ void HatchIntake::SetUpTalons() {
                                        kTimeout_10Millis);
   mPivot.ConfigForwardLimitSwitchSource(LimitSwitchSource_FeedbackConnector, LimitSwitchNormal_NormallyOpen, kTimeout_10Millis);
   mPivot.ConfigReverseLimitSwitchSource(LimitSwitchSource_FeedbackConnector, LimitSwitchNormal_NormallyOpen, kTimeout_10Millis);
-  mPivot.SetSensorPhase(false); // TODO This is different from practice bot. Add a preferene to set this.
+  mPivot.SetSensorPhase(true); // TODO This is different from practice bot. Add a preferene to set this.
   mPivot.SetInverted(true);
   mPivot.ConfigPeakOutputForward(0.1, kTimeout_10Millis);
   mPivot.ConfigPeakOutputReverse(-0.1, kTimeout_10Millis);
@@ -136,9 +136,9 @@ void HatchIntake::SetupMotionMagic()
   constexpr double kP {calcP()};
   constexpr double kI {0};
   constexpr double kD {0};
-  const double kMaxVelocity {1000};//encoderFwd}; // Read as encoderFwd/sec Move from 0 to max forward in 1 sec
+  const double kMaxVelocity {1200};//encoderFwd}; // Read as encoderFwd/sec Move from 0 to max forward in 1 sec
   const double kCruiseVelocity {1000}; //Sensor Units per 100ms
-  const double kMotionAcceleration {400};//kCruiseVelocity * 0.25}; //Sensor Units per 100ms/sec
+  const double kMotionAcceleration {1800};//kCruiseVelocity * 0.25}; //Sensor Units per 100ms/sec
   mPivot.SelectProfileSlot(kSlotIndex, kPID_PrimaryClosedLoop);
   mPivot.Config_kF(kSlotIndex, kF, kTimeout_10Millis);
   mPivot.Config_kP(kSlotIndex, kP, kTimeout_10Millis);
