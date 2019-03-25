4942e41bb42e9985ec8face80f3665d00df4347a
Nicolas Schallock <nics@vandenrobotics.com>
2019-03-24 20:53:38 -0700

    Chassis: Speed up driving

--- src/main/cpp/subsystems/Chassis.cpp
+++ src/main/cpp/subsystems/Chassis.cpp
@@ -8,7 +8,7 @@
 
 const char Chassis::kSubsystemName[] = "Chassis";
 
-constexpr double kDefaultHighGear {0.75};
+constexpr double kDefaultHighGear {0.85};
 
 std::shared_ptr<Chassis> Chassis::self;
 
