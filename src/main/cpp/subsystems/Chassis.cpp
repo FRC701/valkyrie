54aee290fab6434a9261650780f0cf2f7fd990e7
Tamia Valledor <tamiavalledor@gmail.com>
2019-04-03 15:56:05 -0700

    Added GetCurrent For All Motors On Chassis

--- src/main/cpp/subsystems/Chassis.cpp
+++ src/main/cpp/subsystems/Chassis.cpp
@@ -90,12 +90,20 @@ double Chassis::GetRightPosition() {
   return rightEncoder.GetPosition();
 }
 
-double Chassis::GetLeftCurrent() {
+double Chassis::GetLeft1Current() {
   return left1Wheel.GetOutputCurrent();
 }
 
-double Chassis::GetRightCurrent() {
+double Chassis::GetLeft2Current() {
+  return left2Wheel.GetOutputCurrent();
+}
+
+double Chassis::GetRight1Current() {
   return right1Wheel.GetOutputCurrent();
+}
+
+double Chassis::GetRight2Current() {
+  return right2Wheel.GetOutputCurrent();
 }
 
 double Chassis::GetLeftVoltage() {
